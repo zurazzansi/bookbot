5c903d948f4ab9907c362410c7ccc416348e5f36
zurazzansi <jeffrey.leaming@gmail.com>
2023-11-25 13:06:19 +0100

    finished task, ready for submit

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
 
+char_dict = {}
 
 def word_count(file):
     word_count = file.split()
-    print(len(word_count))
+    print("")
+    print(f"Word count: {len(word_count)}")
+    print("")
+
+def char_count(file):
+    for i in file.lower():
+        if i in char_dict:
+            char_dict[i] += 1
+        else:
+            char_dict[i] = 1
+
+    sort_list = list(char_dict.items())
+    newlist = sorted(sort_list, key=lambda d: d[1], reverse=True)
+    print("")
+    print("-" * 80)
+    print("")
+    
+    for t in newlist:
+        if t[0].isalpha():
+            print(f"The '{t[0]}' character was found {t[1]} times")
+    
+    print("")
+    print("-" * 80)
+
 
 word_count(file_contents)
+char_count(file_contents)
